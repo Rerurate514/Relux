e6e8d421c67b74acd92607a76b130c5b58a55ef1
Rerurate514 <rerurateyuto@gmail.com>
2024-11-29 01:32:55 +0900

    vgaテキストモードを実装

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,18 @@ fn panic(_info: &PanicInfo) -> ! {
 	loop{}
 }
 
+static HELLO: &[u8] = b"Hello World!";
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-	loop {}
+	let vga_buffer = 0x8000 as *mut u8;
+
+	for (i, &byte) in HELLO.iter().enumerate() {
+		unsafe {
+			*vga_buffer.offset(i as isize * 2) = byte;
+			*vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+		}
+	}
+
+	loop { }
 }

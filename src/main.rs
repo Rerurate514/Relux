4f66409d32602423df13118023ad9ddf29fb0865
Rerurate514 <rerurateyuto@gmail.com>
2024-12-27 22:56:24 +0900

    mainでのマクロの使用

--- src/main.rs
+++ src/main.rs
@@ -14,10 +14,7 @@ static STRING: &[u8] = b"LIKE A PHOENIX!!";
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-	use core::fmt::Write;
-	vga_buffer::WRITER.lock().write_str("LIKE A PHOENIX!").unwrap();
-	write!(vga_buffer::WRITER.lock(), ", numbers: {} {}", 42, 1.3213).unwrap();
-	vga_buffer::print_something();
+	println!("HELLO WORLD{}", "!");
 
 	loop { }
 }

b69c013275594ae5507e54e96a0f5907b9165681
Rerurate514 <rerurateyuto@gmail.com>
2024-12-28 18:12:19 +0900

    test_runner関数の作成

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,19 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
 
 use core::panic::PanicInfo;
 
 mod vga_buffer;
+
+#[cfg(test)]
+pub fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+}
 
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
